eef0f9767b0afbdf9e7f8b99e462eed955213c13
Vitaly <vitaly.volozhinov.16@um.edu.mt>
2020-10-06 21:31:08 +0200

    Arranged node form 80-char

--- web-installer/src/components/chains/cosmos/forms/nodesForm.js
+++ web-installer/src/components/chains/cosmos/forms/nodesForm.js
@@ -7,7 +7,8 @@ import Divider from '@material-ui/core/Divider';
 import InfoIcon from '@material-ui/icons/Info';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { NEXT, BACK } from '../../../../constants/constants';
-import StepButtonContainer from '../../../../containers/chains/general/stepButtonContainer';
+import StepButtonContainer from
+  '../../../../containers/chains/general/stepButtonContainer';
 import { PingCosmosButton } from '../../../../utils/buttons';
 import { defaultTheme, theme, useStyles } from '../../../theme/default';
 
