9d1c2b44a96e179933a428cca7a46a9e7f80c075
Vitaly <vitaly.volozhinov.16@um.edu.mt>
2020-10-08 16:19:55 +0200

    Changed error codes to be constants

--- web-installer/src/server.js
+++ web-installer/src/server.js
@@ -159,7 +159,7 @@ async function saveRefreshTokenToDB(username, refreshToken) {
   } catch (err) {
     // If the error is already a mongo error no need to wrap it again as a mongo
     // error
-    if (err.code === 442) {
+    if (err.code === C_442) {
       throw err;
     }
     throw new errors.MongoError(err.message);
@@ -363,7 +363,7 @@ app.post('/server/account/save', verify, async (req, res) => {
     // If the record is already present in the database return a username
     // already exists error so that the error is more meaningful. Otherwise
     // return whatever error is thrown.
-    if (err.code === 446) {
+    if (err.code === C_446) {
       const error = new errors.UsernameAlreadyExists(username);
       console.log(error);
       res.status(error.code).send(utils.errorJson(error.message));
@@ -397,7 +397,7 @@ app.post('/server/account/delete', verify, async (req, res) => {
     // If the record is already present in the database return a username
     // already exists error so that the error is more meaningful. Otherwise
     // return whatever error is thrown.
-    if (err.code === 446) {
+    if (err.code === C_446) {
       const error = new errors.UsernameDoesNotExists(username);
       console.log(error);
       res.status(error.code).send(utils.errorJson(error.message));
--- web-installer/src/server/errors.js
+++ web-installer/src/server/errors.js
@@ -1,106 +1,108 @@
+import './constants';
+
 function InvalidConfigType() {
   this.message = 'The config type is invalid. It must either be \'channel\','
     + ' \'chain\' or \'other\'';
-  this.code = 430;
+  this.code = C_430;
 }
 
 function InvalidBaseChain() {
   this.message = 'The base chain is invalid. It must either be \'cosmos\' or '
     + '\'substrate\'';
-  this.code = 431;
+  this.code = C_431;
 }
 
 function MissingArguments(...args) {
   this.message = `Missing argument(s) '${args}'`;
-  this.code = 432;
+  this.code = C_432;
 }
 
 function ConfigUnrecognized(file) {
   this.message = `'${file}' does not belong to the inferred path.`;
-  this.code = 433;
+  this.code = C_433;
 }
 
 function ConfigNotFound(file) {
   this.message = `Cannot find '${file}' in the inferred path.`;
-  this.code = 434;
+  this.code = C_434;
 }
 
 function InvalidEndpoint(endpoint) {
   this.message = `'${endpoint}' is an invalid endpoint.`;
-  this.code = 435;
+  this.code = C_435;
 }
 
 function TwilioError(err) {
   this.message = `Twilio Error: ${err}`;
-  this.code = 436;
+  this.code = C_436;
 }
 
 function EmailError(err) {
   this.message = `Email Error: ${err}`;
-  this.code = 437;
+  this.code = C_437;
 }
 
 function PagerDutyError(err) {
   this.message = `PagerDuty Error: ${err}`;
-  this.code = 438;
+  this.code = C_438;
 }
 
 function OpsGenieError(err) {
   this.message = `OpsGenie Error: ${err}`;
-  this.code = 439;
+  this.code = C_439;
 }
 
 function MissingFile(filepath) {
   this.message = `Cannot find ${filepath}.`;
-  this.code = 440;
+  this.code = C_440;
 }
 
 function AuthenticationError(err) {
   this.message = `Authentication Error: ${err}.`;
-  this.code = 441;
+  this.code = C_441;
 }
 
 function MongoError(err) {
   this.message = `Mongo Error: ${err}`;
-  this.code = 442;
+  this.code = C_442;
 }
 
 function MissingInstallerCredentials(...args) {
   this.message = `Missing installer credential(s) '${args}' in .env. Server`
     + ' will stop.';
-  this.code = 443;
+  this.code = C_443;
 }
 
 function Unauthorized() {
   this.message = 'Not authorized to perform this operation.';
-  this.code = 444;
+  this.code = C_444;
 }
 
 function CouldNotFindRefreshTokenInDB() {
   this.message = 'Could not find refresh token in database.';
-  this.code = 445;
+  this.code = C_445;
 }
 
 function RecordAlreadyExists(keyValue) {
   this.message = `The record with key ${JSON.stringify(keyValue)} already `
     + ' exists in the database.';
-  this.code = 446;
+  this.code = C_446;
 }
 
 function UsernameAlreadyExists(username) {
   this.message = `Username '${username}' already exists.`;
-  this.code = 447;
+  this.code = C_447;
 }
 
 function CouldNotWriteConfig(err, config, path) {
   this.message = `Could not write config '${config}' to '${path}'. Error: `
     + `${err}`;
-  this.code = 448;
+  this.code = C_448;
 }
 
 function UsernameDoesNotExists(username) {
   this.message = `Username '${username}' does not exists.`;
-  this.code = 449;
+  this.code = C_449;
 }
 
 module.exports = {
