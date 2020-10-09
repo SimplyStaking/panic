e7a7e3d2a14de05bd623859372c7a2a0688cb95b
Vitaly <vitaly.volozhinov.16@um.edu.mt>
2020-10-09 16:31:17 +0200

    started working on understanding rabbitmq

---
+++ store.py
@@ -0,0 +1,28 @@
+import os
+import pika
+import pika.exceptions
+import sys
+import time
+
+from alerter.src.utils.logging import DUMMY_LOGGER
+from alerter.src.data_store.mongo.mongo_api import MongoApi
+from alerter.src.data_store.redis.redis_api import RedisApi
+
+if __name__ == '__main__':
+
+    # Testing rabbit
+    rabbit_host = os.environ["RABBIT_HOST"]
+    connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host))
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    print(" [x] Sent 'Hello World!'")
+    connection2 = pika.BlockingConnection(
+        pika.ConnectionParameters(rabbit_host))
+    channel2 = connection2.channel()
+    channel2.queue_declare(queue='hello')
+    channel2.basic_consume(queue='hello', auto_ack=True, on_message_callback=callback)
+    channel2.start_consuming()
+
+    # In the API we need to always open a connection and close it
+    connection.close()
