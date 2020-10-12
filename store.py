2dbfff03a0a0421f5b32aebcfa2c350c118cc06e
Vitaly <vitaly.volozhinov.16@um.edu.mt>
2020-10-12 10:42:52 +0200

    working on store

--- store.py
+++ store.py
@@ -8,15 +8,22 @@ from alerter.src.utils.logging import DUMMY_LOGGER
 from alerter.src.data_store.mongo.mongo_api import MongoApi
 from alerter.src.data_store.redis.redis_api import RedisApi
 
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
 if __name__ == '__main__':
 
     # Testing rabbit
+    # Sending message through RABBIT
     rabbit_host = os.environ["RABBIT_HOST"]
     connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host))
     channel = connection.channel()
     channel.queue_declare(queue='hello')
     channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
     print(" [x] Sent 'Hello World!'")
+    connection.close()
+
+    # Receving message from RABBIT
     connection2 = pika.BlockingConnection(
         pika.ConnectionParameters(rabbit_host))
     channel2 = connection2.channel()
@@ -25,4 +32,4 @@ if __name__ == '__main__':
     channel2.start_consuming()
 
     # In the API we need to always open a connection and close it
-    connection.close()
+    connection2.close()
