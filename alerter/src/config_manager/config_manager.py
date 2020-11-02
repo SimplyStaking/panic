3f987fabf1ae88ca684bd43b83eed02444d852a9
Vitaly <vitaly.volozhinov.16@um.edu.mt>
2020-11-03 00:28:55 +0100

    Put back DEFAULT key

--- alerter/src/config_manager/config_manager.py
+++ alerter/src/config_manager/config_manager.py
@@ -178,7 +178,6 @@ class ConfigManager:
         self._logger.debug("Config read successfully")
 
         config_dict = {key: dict(config[key]) for key in config}
-        del config_dict['DEFAULT']
         self._logger.debug("Config converted to dict: %s", config_dict)
         # Since the watcher is configured to watch files in
         # self._config_directory we only need check that (for get_routing_key)
