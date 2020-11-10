9160e0917470a43e091c5336e72b1fcc24b60c44
Dylan Galea <dylan@simply-vc.com.mt>
2020-11-10 11:34:30 +0100

    Added some structure.

---
+++ alerter/src/channels_manager/channels/channel.py
@@ -0,0 +1,3 @@
+# TODO: All channel manager logic must be done inside the channels manager
+#     : process, including logging, backup channels, alert not sent etc. We may
+#     : even have a queue of alerts for each channel from the outside.
