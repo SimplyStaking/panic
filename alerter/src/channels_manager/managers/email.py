0a746d893b92d3fe66cc2e5f67efbba387a4adc7
Mark Said Camilleri <markscamilleri@users.noreply.github.com>
2020-12-07 00:16:37 +0000

    Starting EmailManager

--- alerter/src/channels_manager/managers/email.py
+++ alerter/src/channels_manager/managers/email.py
@@ -1,4 +1,36 @@
+from logging import Logger
+from types import FrameType
+
+import pika
+from pika.adapters.blocking_connection import BlockingChannel
+
 from src.channels_manager.managers import ChannelManager
 
 
 class EmailChannelManager(ChannelManager):
+    def __init__(self, logger: Logger, rabbit_ip: str):
+        super().__init__(logger, rabbit_ip)
+
+    def _initialise_rabbit(self) -> None:
+        pass
+
+    def _process_config(self, ch: BlockingChannel,
+                        method: pika.spec.Basic.Deliver,
+                        properties: pika.spec.BasicProperties,
+                        body: bytes) -> None:
+        pass
+
+    def _process_ping(self, ch: BlockingChannel,
+                      method: pika.spec.Basic.Deliver,
+                      properties: pika.spec.BasicProperties,
+                      body: bytes) -> None:
+        pass
+
+    def manage(self) -> None:
+        pass
+
+    def start(self) -> None:
+        pass
+
+    def on_terminate(self, signum: int, stack: FrameType) -> None:
+        pass
