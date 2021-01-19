b65c353c8cfd25259677a5b1f3e048400ee3a2d2
Dylan Galea <dylan@simply-vc.com.mt>
2021-01-19 16:51:49 +0100

    Added all unit tests for the monitor.py script. .start() and _on_terminate() were not tested.

--- alerter/test/monitors/test_system.py
+++ alerter/test/monitors/test_system.py
@@ -1,20 +1,25 @@
 import logging
 import unittest
+from datetime import datetime
 from datetime import timedelta
+from unittest import mock
+
+import pika
 
 from src.configs.system import SystemConfig
 from src.message_broker.rabbitmq import RabbitMQApi
 from src.monitors.system import SystemMonitor
 from src.utils.constants import RAW_DATA_EXCHANGE, HEALTH_CHECK_EXCHANGE
+from src.utils.exceptions import PANICException
 
 
 class TestSystemMonitor(unittest.TestCase):
     def setUp(self) -> None:
         self.dummy_logger = logging.getLogger('Dummy')
         self.connection_check_time_interval = timedelta(seconds=0)
-        self.rabbitmq = RabbitMQApi(self.dummy_logger,
-                                    connection_check_time_interval=
-                                    self.connection_check_time_interval)
+        self.rabbitmq = RabbitMQApi(
+            self.dummy_logger,
+            connection_check_time_interval=self.connection_check_time_interval)
         self.monitor_name = 'test_monitor'
         self.monitoring_period = 10
         self.system_id = 'test_system_id'
@@ -22,6 +27,18 @@ class TestSystemMonitor(unittest.TestCase):
         self.system_name = 'test_system'
         self.monitor_system = True
         self.node_exporter_url = 'test_url'
+        self.routing_key = 'test_routing_key'
+        self.test_data_str = 'test data'
+        self.test_data_dict = {
+            'test_key_1': 'test_val_1',
+            'test_key_2': 'test_val_2',
+        }
+        self.test_heartbeat = {
+            'component_name': 'Test Component',
+            'timestamp': datetime.now().timestamp()
+        }
+        self.test_queue_name = 'Test Queue'
+        self.test_exception = PANICException('test_exception', 1)
         self.system_config = SystemConfig(self.system_id, self.parent_id,
                                           self.system_name, self.monitor_system,
                                           self.node_exporter_url)
@@ -32,6 +49,7 @@ class TestSystemMonitor(unittest.TestCase):
     def tearDown(self) -> None:
         self.dummy_logger = None
         self.rabbitmq = None
+        self.test_exception = None
         self.system_config = None
         self.test_monitor = None
 
@@ -60,11 +78,93 @@ class TestSystemMonitor(unittest.TestCase):
             self.rabbitmq.disconnect()
 
             self.test_monitor._initialise_rabbitmq()
+
+            # Perform checks that the connection has been opened, marked as open
+            # and that the delivery confirmation variable is set.
             self.assertTrue(self.test_monitor.rabbitmq.is_connected)
             self.assertTrue(self.test_monitor.rabbitmq.connection.is_open)
-            # TODO: Need to continue doing tests for exchanges and confirm
-            #     : delivery here
+            self.assertTrue(
+                self.test_monitor.rabbitmq.channel._delivery_confirmation)
+
+            # Check whether the exchange has been creating by sending messages
+            # to it. If this fails an exception is raised hence the test fails.
+            self.test_monitor.rabbitmq.basic_publish_confirm(
+                exchange=RAW_DATA_EXCHANGE, routing_key=self.routing_key,
+                body=self.test_data_str, is_body_dict=False,
+                properties=pika.BasicProperties(delivery_mode=2),
+                mandatory=False)
+            self.test_monitor.rabbitmq.basic_publish_confirm(
+                exchange=HEALTH_CHECK_EXCHANGE, routing_key=self.routing_key,
+                body=self.test_data_str, is_body_dict=False,
+                properties=pika.BasicProperties(delivery_mode=2),
+                mandatory=False)
+
             self.test_monitor.rabbitmq.disconnect()
         except Exception as e:
             self.fail("Test failed: {}".format(e))
 
+    @mock.patch.object(SystemMonitor, "_process_retrieved_data")
+    @mock.patch.object(SystemMonitor, "_process_error")
+    def test_process_data_calls_process_error_retrieval_error(
+            self, mock_process_error, mock_process_retrieved_data) -> None:
+        # Do not test the processing of data for now
+        mock_process_error.return_value = self.test_data_dict
+
+        self.test_monitor._process_data(self.test_data_dict, True,
+                                        self.test_exception)
+
+        # Test passes if _process_error is called once and
+        # process_retrieved_data is not called
+        try:
+            self.assertEqual(1, mock_process_error.call_count)
+            self.assertEqual(0, mock_process_retrieved_data.call_count)
+        except AssertionError as e:
+            self.fail("Test failed: {}".format(e))
+
+    @mock.patch.object(SystemMonitor, "_process_retrieved_data")
+    @mock.patch.object(SystemMonitor, "_process_error")
+    def test_process_data_calls_process_retrieved_data_on_retrieval_success(
+            self, mock_process_error, mock_process_retrieved_data) -> None:
+        # Do not test the processing of data for now
+        mock_process_retrieved_data.return_value = self.test_data_dict
+
+        self.test_monitor._process_data(self.test_data_dict, False, None)
+
+        # Test passes if _process_error is called once and
+        # process_retrieved_data is not called
+        try:
+            self.assertEqual(0, mock_process_error.call_count)
+            self.assertEqual(1, mock_process_retrieved_data.call_count)
+        except AssertionError as e:
+            self.fail("Test failed: {}".format(e))
+
+    def test_send_heartbeat_sends_a_heartbeat_correctly(self) -> None:
+        # This test creates a queue which receives messages with the same
+        # routing key as the ones sent by send_heartbeat, and checks that the
+        # heartbeat is received
+        try:
+            self.test_monitor._initialise_rabbitmq()
+
+            res = self.test_monitor.rabbitmq.queue_declare(
+                queue=self.test_queue_name, durable=True, exclusive=False,
+                auto_delete=False, passive=False
+            )
+            self.assertEqual(0, res.method.message_count)
+            self.test_monitor.rabbitmq.queue_bind(
+                queue=self.test_queue_name, exchange=HEALTH_CHECK_EXCHANGE,
+                routing_key='heartbeat.worker')
+            self.test_monitor._send_heartbeat(self.test_heartbeat)
+
+            # By re-declaring the queue again we can get the number of messages
+            # in the queue.
+            res = self.test_monitor.rabbitmq.queue_declare(
+                queue=self.test_queue_name, durable=True, exclusive=False,
+                auto_delete=False, passive=True
+            )
+            self.assertEqual(1, res.method.message_count)
+
+            # Clean before test finishes
+            self.test_monitor.rabbitmq.queue_delete(self.test_queue_name)
+            self.test_monitor.rabbitmq.disconnect()
+        except Exception as e:
+            self.fail("Test failed: {}".format(e))
