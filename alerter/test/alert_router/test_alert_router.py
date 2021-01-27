9ed488ac9d92a8534720cfba31e8dd1894db283b
Mark Said Camilleri <markscamilleri@users.noreply.github.com>
2021-01-27 15:42:33 +0000

    Finished teardown

--- alerter/test/alert_router/test_alert_router.py
+++ alerter/test/alert_router/test_alert_router.py
@@ -2,6 +2,8 @@ import logging
 import unittest
 from datetime import timedelta
 
+import pika.exceptions
+
 from src.alert_router.alert_router import AlertRouter
 from src.data_store.redis import RedisApi
 from src.message_broker.rabbitmq import RabbitMQApi
@@ -46,15 +48,24 @@ class TestAlertRouter(unittest.TestCase):
         }
 
         self._test_alert_router = AlertRouter(
-            "Alert Router", self._alert_router_logger, self._rabbit_ip, self._redis_ip,
+            "Alert Router", self._alert_router_logger, self._rabbit_ip,
+            self._redis_ip,
             self._redis_db, self._redis_port, "test_alerter", True, True
         )
 
     def tearDown(self) -> None:
         # flush and consume all from rabbit queues and exchanges
         self._rabbitmq.connect_till_successful()
-        self._rabbitmq.queue_purge(ALERT_ROUTER_CONFIGS_QUEUE_NAME)
-        self._rabbitmq.queue_delete(ALERT_ROUTER_CONFIGS_QUEUE_NAME)
+
+        try:
+            self._rabbitmq.queue_declare(ALERT_ROUTER_CONFIGS_QUEUE_NAME,
+                                         passive=True)
+            self._rabbitmq.queue_purge(ALERT_ROUTER_CONFIGS_QUEUE_NAME)
+            self._rabbitmq.queue_delete(ALERT_ROUTER_CONFIGS_QUEUE_NAME)
+        except pika.exceptions.ChannelClosedByBroker as e:
+            print("Queue {} does not exist - don't need to close".format(
+                ALERT_ROUTER_CONFIGS_QUEUE_NAME
+            ))
 
         exchanges = [
             ALERT_EXCHANGE, CONFIG_EXCHANGE, HEALTH_CHECK_EXCHANGE,
@@ -62,9 +73,50 @@ class TestAlertRouter(unittest.TestCase):
         ]
 
         for exchange in exchanges:
-            self._rabbitmq.channel.exchange_delete(exchange)
+            try:
+                self._rabbitmq.exchange_declare(exchange, passive=True)
+                self._rabbitmq.channel.exchange_delete(exchange)
+            except pika.exceptions.ChannelClosedByBroker as e:
+                print("Exchange {} does not exist - don't need to close".format(
+                    exchange))
 
         self._rabbitmq.disconnect_till_successful()
 
     def test_alert_router_initialised(self):
         self.assertIsNotNone(self._test_alert_router)
+
+    def test_name(self):
+        self.assertEqual("Alert Router", self._test_alert_router.name)
+
+    def test__initialise_rabbit(self):
+        self.fail()
+
+    def test__declare_exchange_and_bind_queue(self):
+        self.fail()
+
+    def test__process_configs(self):
+        self.fail()
+
+    def test__process_alert(self):
+        self.fail()
+
+    def test__process_ping(self):
+        self.fail()
+
+    def test_start(self):
+        self.fail()
+
+    def test_disconnect_from_rabbit(self):
+        self.fail()
+
+    def test_on_terminate(self):
+        self.fail()
+
+    def test__extract_config(self):
+        self.fail()
+
+    def test_is_all_muted(self):
+        self.fail()
+
+    def test_is_chain_severity_muted(self):
+        self.fail()
