a736fa233f01278c8056c48d5dd2355aef505b4b
Mark Said Camilleri <markscamilleri@users.noreply.github.com>
2021-02-17 23:50:05 +0000

    Testing is_recently_disconnected and _safe

--- alerter/test/message_broker/rabbitmq/test_rabbitmq_api.py
+++ alerter/test/message_broker/rabbitmq/test_rabbitmq_api.py
@@ -1,9 +1,15 @@
+import copy
 import logging
 from datetime import timedelta
-from unittest import TestCase, skip
+from typing import List, Any
+from unittest import TestCase, skip, mock
+from unittest.mock import MagicMock, PropertyMock
+
+from parameterized import parameterized
 
 from src.message_broker.rabbitmq import RabbitMQApi
 from src.utils import env
+from src.utils.timing import TimedTaskLimiter
 
 
 class TestRabbitMQApi(TestCase):
@@ -27,10 +33,10 @@ class TestRabbitMQApi(TestCase):
         # correctly
         username = 'test_username'
         password = 'test_password'
-        connection_check_time_interval = timedelta(seconds=100)
+        connection_check_seconds = 100
         rabbit = RabbitMQApi(
             self.rabbit_logger, self.rabbit_ip, self.rabbit_port, username,
-            password, connection_check_time_interval
+            password, timedelta(seconds=connection_check_seconds)
         )
 
         self.assertIsNotNone(rabbit)
@@ -39,28 +45,97 @@ class TestRabbitMQApi(TestCase):
         self.assertEqual(rabbit._port, self.rabbit_port)
         self.assertEqual(rabbit.username, username)
         self.assertEqual(rabbit.password, password)
-        self.assertEqual(connection_check_time_interval,
-                         connection_check_time_interval)
+        self.assertEqual(connection_check_seconds,
+                         rabbit.connection_check_time_interval_seconds)
+        self.assertEqual(
+            connection_check_seconds,
+            rabbit.live_check_limiter.time_interval.total_seconds()
+        )
         self.assertFalse(rabbit.is_connected)
 
+    @parameterized.expand([
+        ("started with true", True), ("started with false", False)
+    ])
+    def test_set_as_connected_sets_connected_as_true(
+            self, name: str, starting_value: bool
+    ):
+        self.rabbit._is_connected = starting_value
+        self.rabbit._set_as_connected()
+        self.assertTrue(self.rabbit.is_connected)
+
+    @parameterized.expand([
+        ("started with true", True), ("started with false", False)
+    ])
+    def test_set_as_disconnected_sets_connected_as_false(
+            self, name: str, starting_value: bool
+    ):
+        self.rabbit._is_connected = starting_value
+        self.rabbit._set_as_disconnected()
+        self.assertFalse(self.rabbit.is_connected)
+
+    @parameterized.expand([
+        ("is connected and can do task return false", True, True, False),
+        ("is connected and cannot do task return false", True, False, False),
+        ("is not connected and can do task return false", False, True, False),
+        ("is not connected and cannot do task return true", False, False, True)
+    ])
+    @mock.patch.object(RabbitMQApi, "is_connected",
+                       new_callable=mock.PropertyMock)
+    @mock.patch.object(TimedTaskLimiter, "can_do_task", autospec=True)
+    def test_is_recently_disconnected(
+            self, name: str, is_connected_mock_value: bool,
+            can_do_task_value: bool, expected_output: bool,
+            mock_can_do_task: MagicMock, mock_is_connected: PropertyMock
+    ):
+        print(type(mock_is_connected))
+        mock_is_connected.return_value = is_connected_mock_value
+        mock_can_do_task.return_value = can_do_task_value
+        self.assertEqual(
+            expected_output, self.rabbit._is_recently_disconnected()
+        )
+
+    @parameterized.expand([
+        ("multiple string inputs and string output",
+         ["first_input", "second_input"], "output"),
+        ("single string input and int output", ["only input"], 10),
+        ("bool and int inputs and bool output", [True, 1], False),
+        ("none input none output", [None], None)
+    ])
+    @mock.patch.object(RabbitMQApi, "_is_recently_disconnected")
+    def test_safe_function_executes_with_no_errors_and_returns_correctly(
+            self, name: str, function_arguments: List[Any],
+            function_output: Any, mock_is_recently_disconnected: MagicMock
+    ):
+        mock_function_to_execute = MagicMock(return_value=function_output)
+        mock_is_recently_disconnected.return_value = False
+
+        self.assertEquals(
+            function_output,
+            self.rabbit._safe(
+                mock_function_to_execute, function_arguments, "DEFAULT"
+            )
+        )
+        mock_function_to_execute.assert_called_once_with(*function_arguments)
+        mock_is_recently_disconnected.assert_called_once()
+
     @skip("Not implemented yet")
-    def test_live_check_limiter(self):
+    def test_safe_function_returns_default_if_recently_disconnected(self):
         pass
 
     @skip("Not implemented yet")
-    def test__set_as_connected(self):
+    def test_safe_function_on_unroutable_error_raises(self):
         pass
 
     @skip("Not implemented yet")
-    def test__set_as_disconnected(self):
+    def test_safe_function_on_channel_error_raises(self):
         pass
 
     @skip("Not implemented yet")
-    def test__is_recently_disconnected(self):
+    def test_safe_function_on_connection_error_raises(self):
         pass
 
     @skip("Not implemented yet")
-    def test__safe(self):
+    def test_safe_function_on_other_exception_raises(self):
         pass
 
     @skip("Not implemented yet")
