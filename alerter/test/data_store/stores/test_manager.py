e837b2a696e0bdda2a48b48094f6d15291cd206f
VitalyVolozhinov <vitalyvolozhinov@gmail.com>
2021-02-22 15:42:38 +0100

    Completed StoreManager tests

--- alerter/test/data_store/stores/test_manager.py
+++ alerter/test/data_store/stores/test_manager.py
@@ -20,7 +20,8 @@ from src.utils.constants import (HEALTH_CHECK_EXCHANGE, SYSTEM_STORE_NAME,
                                  GITHUB_STORE_NAME, ALERT_STORE_NAME,
                                  DATA_STORE_MAN_INPUT_QUEUE,
                                  DATA_STORE_MAN_INPUT_ROUTING_KEY)
-from src.utils.exceptions import MessageWasNotDeliveredException
+from src.utils.exceptions import (MessageWasNotDeliveredException,
+                                  PANICException)
 from src.utils.logging import log_and_print
 from src.utils import env
 
@@ -67,6 +68,7 @@ class TestStoreManager(unittest.TestCase):
             'component_name': self.manager_name,
             'timestamp': datetime(2012, 1, 1).timestamp(),
         }
+        self.test_exception = PANICException('test_exception', 1)
 
     def tearDown(self) -> None:
         connect_to_rabbit(self.rabbitmq)
@@ -161,6 +163,8 @@ class TestStoreManager(unittest.TestCase):
 
         self.test_store_manager._start_stores_processes()
 
+        time.sleep(1)
+
         new_entry_process = self.test_store_manager._store_process_dict[
             SYSTEM_STORE_NAME]
 
@@ -175,6 +179,8 @@ class TestStoreManager(unittest.TestCase):
 
         self.test_store_manager._start_stores_processes()
 
+        time.sleep(1)
+
         new_entry_process = self.test_store_manager._store_process_dict[
             GITHUB_STORE_NAME]
 
@@ -188,6 +194,8 @@ class TestStoreManager(unittest.TestCase):
         mock_start.return_value = None
 
         self.test_store_manager._start_stores_processes()
+
+        time.sleep(1)
 
         new_entry_process = self.test_store_manager._store_process_dict[
             ALERT_STORE_NAME]
@@ -225,7 +233,7 @@ class TestStoreManager(unittest.TestCase):
         new_alert_process.join()
 
     @freeze_time("2012-01-01")
-    @mock.patch("src.alerter.alerter_starters.create_logger")
+    @mock.patch("src.data_store.starters.create_logger")
     @mock.patch.object(RabbitMQApi, "basic_ack")
     def test_process_ping_sends_a_valid_hb_if_process_is_alive(
             self, mock_ack, mock_create_logger) -> None:
@@ -235,34 +243,36 @@ class TestStoreManager(unittest.TestCase):
         mock_create_logger.return_value = self.dummy_logger
         mock_ack.return_value = None
         try:
-            self.test_manager._initialise_rabbitmq()
-            self.test_manager._start_alerters_processes()
+            self.test_store_manager._initialise_rabbitmq()
+            self.test_store_manager._start_stores_processes()
 
             # Give time for the processes to start
             time.sleep(1)
 
             # Delete the queue before to avoid messages in the queue on error.
-            self.test_manager.rabbitmq.queue_delete(self.test_queue_name)
+            self.test_rabbit_manager.queue_delete(self.test_queue_name)
 
             # initialise
-            blocking_channel = self.test_manager.rabbitmq.channel
+            blocking_channel = self.test_store_manager.rabbitmq.channel
             properties = pika.spec.BasicProperties()
-            method_hb = pika.spec.Basic.Deliver(routing_key='heartbeat.manager')
+            method_hb = pika.spec.Basic.Deliver(routing_key=self.routing_key)
             body = 'ping'
-            res = self.test_manager.rabbitmq.queue_declare(
+            res = self.test_rabbit_manager.queue_declare(
                 queue=self.test_queue_name, durable=True, exclusive=False,
                 auto_delete=False, passive=False
             )
             self.assertEqual(0, res.method.message_count)
-            self.test_manager.rabbitmq.queue_bind(
+            self.test_rabbit_manager.queue_bind(
                 queue=self.test_queue_name, exchange=HEALTH_CHECK_EXCHANGE,
-                routing_key='heartbeat.manager')
-            self.test_manager._process_ping(blocking_channel, method_hb,
-                                            properties, body)
+                routing_key=self.routing_key)
+            self.test_store_manager._process_ping(blocking_channel, method_hb,
+                                                  properties, body)
+
+            time.sleep(1)
 
             # By re-declaring the queue again we can get the number of messages
             # in the queue.
-            res = self.test_manager.rabbitmq.queue_declare(
+            res = self.test_rabbit_manager.queue_declare(
                 queue=self.test_queue_name, durable=True, exclusive=False,
                 auto_delete=False, passive=True
             )
@@ -270,17 +280,277 @@ class TestStoreManager(unittest.TestCase):
             expected_output = {
                 "component_name": self.manager_name,
                 "dead_processes": [],
-                "running_processes": [GITHUB_ALERTER_NAME],
+                "running_processes": [SYSTEM_STORE_NAME, GITHUB_STORE_NAME,
+                                      ALERT_STORE_NAME],
                 "timestamp": datetime(2012, 1, 1).timestamp()
             }
             # Check that the message received is a valid HB
-            _, _, body = self.test_manager.rabbitmq.basic_get(
+            _, _, body = self.test_rabbit_manager.basic_get(
                 self.test_queue_name)
             self.assertEqual(expected_output, json.loads(body))
 
             # Clean before test finishes
-            self.test_manager.alerter_process_dict[
-                GITHUB_ALERTER_NAME].terminate()
-            self.test_manager.alerter_process_dict[GITHUB_ALERTER_NAME].join()
+            self.test_store_manager._store_process_dict[
+                SYSTEM_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                GITHUB_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                ALERT_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                SYSTEM_STORE_NAME].join()
+            self.test_store_manager._store_process_dict[
+                GITHUB_STORE_NAME].join()
+            self.test_store_manager._store_process_dict[
+                ALERT_STORE_NAME].join()
+        except Exception as e:
+            self.fail("Test failed: {}".format(e))
+
+    @freeze_time("2012-01-01")
+    @mock.patch("src.data_store.starters.create_logger")
+    @mock.patch.object(RabbitMQApi, "basic_ack")
+    def test_process_ping_sends_a_valid_hb_if_all_processes_are_dead(
+            self, mock_ack, mock_create_logger) -> None:
+        # This test creates a queue which receives messages with the same
+        # routing key as the ones sent by send_heartbeat, and checks that the
+        # received heartbeat is valid.
+        mock_create_logger.return_value = self.dummy_logger
+        mock_ack.return_value = None
+        try:
+            self.test_store_manager._initialise_rabbitmq()
+            self.test_store_manager._start_stores_processes()
+
+            # Give time for the processes to start
+            time.sleep(1)
+
+            self.test_store_manager._store_process_dict[
+                SYSTEM_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                GITHUB_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                ALERT_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                SYSTEM_STORE_NAME].join()
+            self.test_store_manager._store_process_dict[
+                GITHUB_STORE_NAME].join()
+            self.test_store_manager._store_process_dict[
+                ALERT_STORE_NAME].join()
+
+            # Time for processes to terminate
+            time.sleep(1)
+
+            # Delete the queue before to avoid messages in the queue on error.
+            self.test_rabbit_manager.queue_delete(self.test_queue_name)
+
+            # initialise
+            blocking_channel = self.test_store_manager.rabbitmq.channel
+            properties = pika.spec.BasicProperties()
+            method_hb = pika.spec.Basic.Deliver(routing_key=self.routing_key)
+            body = 'ping'
+            res = self.test_rabbit_manager.queue_declare(
+                queue=self.test_queue_name, durable=True, exclusive=False,
+                auto_delete=False, passive=False
+            )
+            self.assertEqual(0, res.method.message_count)
+            self.test_rabbit_manager.queue_bind(
+                queue=self.test_queue_name, exchange=HEALTH_CHECK_EXCHANGE,
+                routing_key=self.routing_key)
+            self.test_store_manager._process_ping(blocking_channel, method_hb,
+                                                  properties, body)
+
+            time.sleep(1)
+
+            # By re-declaring the queue again we can get the number of messages
+            # in the queue.
+            res = self.test_rabbit_manager.queue_declare(
+                queue=self.test_queue_name, durable=True, exclusive=False,
+                auto_delete=False, passive=True
+            )
+            self.assertEqual(1, res.method.message_count)
+            expected_output = {
+                "component_name": self.manager_name,
+                "dead_processes": [SYSTEM_STORE_NAME, GITHUB_STORE_NAME,
+                                   ALERT_STORE_NAME],
+                "running_processes": [],
+                "timestamp": datetime(2012, 1, 1).timestamp()
+            }
+            # Check that the message received is a valid HB
+            _, _, body = self.test_rabbit_manager.basic_get(
+                self.test_queue_name)
+            self.assertEqual(expected_output, json.loads(body))
+
+            # Clean before test finishes
+            self.test_store_manager._store_process_dict[
+                SYSTEM_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                GITHUB_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                ALERT_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                SYSTEM_STORE_NAME].join()
+            self.test_store_manager._store_process_dict[
+                GITHUB_STORE_NAME].join()
+            self.test_store_manager._store_process_dict[
+                ALERT_STORE_NAME].join()
+            self.rabbitmq.disconnect()
+        except Exception as e:
+            self.fail("Test failed: {}".format(e))
+
+    @freeze_time("2012-01-01")
+    @mock.patch.object(RabbitMQApi, "basic_ack")
+    @mock.patch("src.data_store.starters.create_logger")
+    @mock.patch.object(StoreManager, "_send_heartbeat")
+    def test_process_ping_restarts_dead_processes(
+            self, send_hb_mock, mock_create_logger, mock_ack) -> None:
+        send_hb_mock.return_value = None
+        mock_create_logger.return_value = self.dummy_logger
+        mock_ack.return_value = None
+        try:
+            self.test_store_manager._initialise_rabbitmq()
+            self.test_store_manager._start_stores_processes()
+
+            # Give time for the processes to start
+            time.sleep(1)
+
+            self.test_store_manager._store_process_dict[
+                SYSTEM_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                GITHUB_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                ALERT_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                SYSTEM_STORE_NAME].join()
+            self.test_store_manager._store_process_dict[
+                GITHUB_STORE_NAME].join()
+            self.test_store_manager._store_process_dict[
+                ALERT_STORE_NAME].join()
+
+            # Give time for the processes to terminate
+            time.sleep(1)
+
+            # Check that that the processes have terminated
+            self.assertFalse(self.test_store_manager._store_process_dict[
+                                SYSTEM_STORE_NAME].is_alive())
+            self.assertFalse(self.test_store_manager._store_process_dict[
+                                GITHUB_STORE_NAME].is_alive())
+            self.assertFalse(self.test_store_manager._store_process_dict[
+                                ALERT_STORE_NAME].is_alive())
+
+            # initialise
+            blocking_channel = self.test_store_manager.rabbitmq.channel
+            properties = pika.spec.BasicProperties()
+            method_hb = pika.spec.Basic.Deliver(routing_key=self.routing_key)
+            body = 'ping'
+            self.test_store_manager._process_ping(blocking_channel, method_hb,
+                                                  properties, body)
+
+            # Give time for the processes to start
+            time.sleep(1)
+
+            self.assertTrue(self.test_store_manager._store_process_dict[
+                                SYSTEM_STORE_NAME].is_alive())
+            self.assertTrue(self.test_store_manager._store_process_dict[
+                                GITHUB_STORE_NAME].is_alive())
+            self.assertTrue(self.test_store_manager._store_process_dict[
+                                ALERT_STORE_NAME].is_alive())
+
+            # Clean before test finishes
+            self.test_store_manager._store_process_dict[
+                SYSTEM_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                GITHUB_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                ALERT_STORE_NAME].terminate()
+            self.test_store_manager._store_process_dict[
+                SYSTEM_STORE_NAME].join()
+            self.test_store_manager._store_process_dict[
+                GITHUB_STORE_NAME].join()
+            self.test_store_manager._store_process_dict[
+                ALERT_STORE_NAME].join()
+        except Exception as e:
+            self.fail("Test failed: {}".format(e))
+
+    @mock.patch.object(multiprocessing.Process, "is_alive")
+    def test_process_ping_does_not_send_hb_if_processing_fails(
+            self, is_alive_mock) -> None:
+        # This test creates a queue which receives messages with the same
+        # routing key as the ones sent by send_heartbeat. In this test we will
+        # check that no heartbeat is sent when mocking a raised exception.
+        is_alive_mock.side_effect = self.test_exception
+        try:
+            self.test_store_manager._initialise_rabbitmq()
+            self.test_store_manager._start_stores_processes()
+
+            time.sleep(1)
+            # Delete the queue before to avoid messages in the queue on error.
+            self.test_rabbit_manager.queue_delete(self.test_queue_name)
+
+            # initialise
+            blocking_channel = self.test_store_manager.rabbitmq.channel
+            method = pika.spec.Basic.Deliver(routing_key=self.routing_key)
+            properties = pika.spec.BasicProperties()
+            body = 'ping'
+            res = self.test_rabbit_manager.queue_declare(
+                queue=self.test_queue_name, durable=True, exclusive=False,
+                auto_delete=False, passive=False
+            )
+            self.assertEqual(0, res.method.message_count)
+            self.test_rabbit_manager.queue_bind(
+                queue=self.test_queue_name, exchange=HEALTH_CHECK_EXCHANGE,
+                routing_key=self.routing_key)
+            self.test_store_manager._process_ping(blocking_channel, method,
+                                                  properties, body)
+
+            time.sleep(1)
+            # By re-declaring the queue again we can get the number of messages
+            # in the queue.
+            res = self.test_rabbit_manager.queue_declare(
+                queue=self.test_queue_name, durable=True, exclusive=False,
+                auto_delete=False, passive=True
+            )
+            self.assertEqual(0, res.method.message_count)
+        except Exception as e:
+            self.fail("Test failed: {}".format(e))
+
+    def test_proc_ping_send_hb_does_not_raise_msg_not_del_exce_if_hb_not_routed(
+            self) -> None:
+        try:
+            self.test_store_manager._initialise_rabbitmq()
+            self.test_store_manager._start_stores_processes()
+
+            time.sleep(1)
+
+            # initialise
+            blocking_channel = self.test_store_manager.rabbitmq.channel
+            method = pika.spec.Basic.Deliver(routing_key='heartbeat.manager')
+            properties = pika.spec.BasicProperties()
+            body = 'ping'
+
+            self.test_store_manager._process_ping(blocking_channel, method,
+                                                  properties, body)
+        except Exception as e:
+            self.fail("Test failed: {}".format(e))
+
+    @parameterized.expand([
+        ("pika.exceptions.AMQPChannelError('test')",
+         "pika.exceptions.AMQPChannelError"),
+        ("self.test_exception", "PANICException"),
+    ])
+    @mock.patch.object(StoreManager, "_send_heartbeat")
+    def test_process_ping_send_hb_raises_exceptions(
+            self, param_input, param_expected, hb_mock) -> None:
+        hb_mock.side_effect = eval(param_input)
+        try:
+            self.test_store_manager._initialise_rabbitmq()
+
+            # initialise
+            blocking_channel = self.test_store_manager.rabbitmq.channel
+            method = pika.spec.Basic.Deliver(routing_key=self.routing_key)
+            properties = pika.spec.BasicProperties()
+            body = 'ping'
+
+            self.assertRaises(eval(param_expected),
+                              self.test_store_manager._process_ping,
+                              blocking_channel,
+                              method, properties, body)
         except Exception as e:
             self.fail("Test failed: {}".format(e))
