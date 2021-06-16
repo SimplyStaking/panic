73c74faf3ccb4f7233376b7c0b9a35978a964b68
Dylan Galea <dylan@simply-vc.com.mt>
2021-06-16 14:29:07 +0200

    Added some MR suggestions.

--- alerter/test/utils/utils.py
+++ alerter/test/utils/utils.py
@@ -76,9 +76,8 @@ def delete_queue_if_exists(rabbit: RabbitMQApi, queue_name: str) -> None:
         rabbit.queue_purge(queue_name)
         rabbit.queue_delete(queue_name)
     except pika.exceptions.ChannelClosedByBroker:
-        print("Queue {} does not exist - don't need to close".format(
-            queue_name
-        ))
+        rabbit.logger.debug(
+            "Queue {} does not exist - don't need to close".format(queue_name))
 
 
 def delete_exchange_if_exists(rabbit: RabbitMQApi, exchange_name: str) -> None:
@@ -86,8 +85,9 @@ def delete_exchange_if_exists(rabbit: RabbitMQApi, exchange_name: str) -> None:
         rabbit.exchange_declare(exchange_name, passive=True)
         rabbit.exchange_delete(exchange_name)
     except pika.exceptions.ChannelClosedByBroker:
-        print("Exchange {} does not exist - don't need to close".format(
-            exchange_name))
+        rabbit.logger.debug(
+            "Exchange {} does not exist - don't need to close".format(
+                exchange_name))
 
 
 def disconnect_from_rabbit(rabbit: RabbitMQApi, attempts: int = 3) -> None:
@@ -99,9 +99,9 @@ def disconnect_from_rabbit(rabbit: RabbitMQApi, attempts: int = 3) -> None:
             return
         except Exception as e:
             tries += 1
-            print("Could not disconnect to rabbit. Attempts so "
-                  "far: {}".format(tries))
-            print(e)
+            rabbit.logger.debug("Could not disconnect to rabbit. Attempts so "
+                                "far: {}".format(tries))
+            rabbit.logger.debug(e)
             if tries >= attempts:
                 raise e
 
@@ -115,9 +115,9 @@ def connect_to_rabbit(rabbit: RabbitMQApi, attempts: int = 3) -> None:
             return
         except Exception as e:
             tries += 1
-            print("Could not connect to rabbit. Attempts so far: {}".format(
-                tries))
-            print(e)
+            rabbit.logger.debug("Could not disconnect to rabbit. Attempts so "
+                                "far: {}".format(tries))
+            rabbit.logger.debug(e)
             if tries >= attempts:
                 raise e
 
