60215930ce930acc5dbecca5b54066658496bfe2
Vitaly Volozhinov <vitalyvolozhinov@gmail.com>
2021-10-05 14:27:35 +0200

    Updating images

--- web-installer/src/components/welcome/welcomePage.js
+++ web-installer/src/components/welcome/welcomePage.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Grid, Button } from '@material-ui/core';
 import LoginContainer from 'containers/welcome/loginContainer';
-import CustomParticles from 'components/material_ui/Particles/BlockchainParticles';
+import Background from 'assets/img/backgrounds/jake-wetton-4RiNCJiOrHM-unsplash.jpg';
 
 function WelcomePage() {
   return (
     <div>
       <Grid container style={{ minHeight: '100vh' }}>
-        <Grid item xs={12} sm={6}>
-          <div>
-            <CustomParticles />
-          </div>
-        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          style={{
+            backgroundImage: `url(${Background})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        />
         <Grid
           container
           item
@@ -28,18 +33,12 @@ function WelcomePage() {
           <div />
           <Grid container justify="center" spacing={2}>
             <Grid item>
-              <Button
-                href="https://simply-vc.com.mt/"
-                target="_blank"
-              >
+              <Button href="https://simply-vc.com.mt/" target="_blank">
                 Visit the SimplyVC Website!
               </Button>
             </Grid>
             <Grid item>
-              <Button
-                href="https://twitter.com/Simply_VC"
-                target="_blank"
-              >
+              <Button href="https://twitter.com/Simply_VC" target="_blank">
                 Follow us on Twitter!
               </Button>
             </Grid>
