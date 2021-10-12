fb3ffdf37642d1bf0f3bfb56bf7569155dc5d1d0
Daniel Cherrett <cherrettdaniel@gmail.com>
2021-10-12 11:01:24 +0200

    Updated CSS and text-title for new filter

--- ui/src/components/panic-alerts-overview/panic-alerts-overview.tsx
+++ ui/src/components/panic-alerts-overview/panic-alerts-overview.tsx
@@ -35,6 +35,19 @@ export class PanicAlertsOverview implements PanicAlertsOverviewInterface {
     }
   }
 
+  async componentDidLoad() {
+    // Chain Filter text-placeholder (Chains).
+    const chainFilterShadow = document.querySelector('#chains-filter ion-select').shadowRoot;
+    const chainFilterSelectText = chainFilterShadow.querySelector('.select-text');
+    const chainFilterSelectIcon = chainFilterShadow.querySelector('.select-icon');
+    const selectTextTitle = chainFilterSelectText.cloneNode() as Element;
+    selectTextTitle.classList.remove('select-text');
+    selectTextTitle.classList.add('select-text-title');
+    selectTextTitle.setAttribute('part', 'text-title');
+    selectTextTitle.textContent = 'Chains';
+    chainFilterShadow.insertBefore(selectTextTitle, chainFilterSelectIcon);
+  }
+
   // Used to keep track of the last clicked column index and the order of the
   // sorted column within the data table (and base chain since correlated).
   @Listen("svcDataTable__lastClickedColumnIndexEvent")
@@ -50,7 +63,7 @@ export class PanicAlertsOverview implements PanicAlertsOverviewInterface {
         <svc-card class="panic-alerts-overview__chain-card">
           <div slot='content' id='expanded' class="panic-alerts-overview__data-table-container">
             <svc-filter event-name="filter-changed" debounce={100}>
-              <div class="panic-dashboard-overview__chain-filter-container">
+              <div class="panic-alerts-overview__chain-filter-container">
                 {/* Chain filter */}
                 <svc-select
                   name="selected-chains"
