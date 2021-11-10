79cc217041a855f308f2e29f92c7070dd1ac7c7e
Vitaly Volozhinov <vitalyvolozhinov@gmail.com>
2021-11-10 20:51:28 +0100

    Persisting state

--- web-installer/src/index.js
+++ web-installer/src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import store from './redux/store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store/store';
 
 import './index.css';
 import App from './App';
 
 // Uncomment to be able to access the store from Chrome console.
-window.store = store;
+// window.store = store;
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
